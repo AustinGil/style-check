37167cc3e29a41da6190ab3fad2950e208accb2b
Austin Gil <austin@stegosource.com>
2017-10-14 15:39:21 -0700

    Lising added styles

--- src/components/Styles.vue
+++ src/components/Styles.vue
@@ -1,21 +1,25 @@
 <template>
 	<div class="styles">
-		<form>
-			<label>Add New Source
-				<input type="url" v-model="srcUrl">
-			</label>
-			<button type="submit" @click.prevent="fetchStylesheet">Add</button>
-		</form>
-		<ul>
-			<!-- <li v-for="(stylesheet, index) in stylesheets" :key="index"> -->
-			<!-- Stylesheet -->
-			<!-- </li> -->
-		</ul>
-
+		<div class="styles__form">
+			<form>
+				<label>Add New Source
+					<input type="url" v-model="srcUrl">
+				</label>
+				<button type="submit" @click.prevent="fetchStylesheet">Add</button>
+			</form>
+		</div>
+		<div class="styles__list">
+			<ul>
+				<li v-for="(stylesheet, index) in stylesheets" :key="index">
+					{{stylesheet.name + ' - ' || ''}}{{stylesheet.url}}
+				</li>
+			</ul>
+		</div>
 	</div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
 import axios from 'axios'
 
 export default {
@@ -24,21 +28,26 @@ export default {
 			srcUrl: ''
 		}
 	},
+	computed: {
+		...mapState([
+			'stylesheets'
+		])
+	},
 	methods: {
 		fetchStylesheet(url) {
-			const tempUrl = 'https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css';
+			// const tempUrl = 'https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css';
 
-			axios.get(tempUrl)
-				.then(function(response) {
-					console.log(response.data);
+			// axios.get(tempUrl)
+			// 	.then(function(response) {
+			// 		// console.log(response.data);
 
-					var headHTML = document.querySelector('.html')[0].innerHTML;
-					headHTML += '<link type="text/css" rel="stylesheet" href="style.css">';
-					document.getElementsByTagName('head')[0].innerHTML = headHTML;
-				})
-				.catch(function(error) {
-					console.log(error);
-				});
+			// 		var headHTML = document.querySelector('.html')[0].innerHTML;
+			// 		headHTML += '<link type="text/css" rel="stylesheet" href="style.css">';
+			// 		document.getElementsByTagName('head')[0].innerHTML = headHTML;
+			// 	})
+			// 	.catch(function(error) {
+			// 		console.log(error);
+			// 	});
 		}
 	}
 }
@@ -48,5 +57,9 @@ export default {
 .styles {
 	max-width: 250px;
 	padding: 10px;
+}
+
+.styles__form {
+	margin-bottom: 20px;
 }
 </style>
