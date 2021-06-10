8e44c244176e2af32b6ea2eb4140747ad9f01b85
Austin Gil <austin@stegosource.com>
2021-06-10 12:09:18 -0700

    Updating meta tags

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,15 @@
+const pkg = require('./package.json')
+const title = 'Style Check'
 module.exports = {
   lintOnSave: false,
   pages: {
     index: {
       entry: 'src/main.js',
-      title: 'Style Check',
+      title: title,
+      encodedTitle: encodeURI(title),
+      description: pkg.description,
+      author: pkg.author.name,
+      homepage: pkg.homepage
     },
   }
 }
